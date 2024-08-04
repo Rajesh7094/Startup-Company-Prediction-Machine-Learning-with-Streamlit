587ed36e35a41188ec7eb98aabf8a8457d6f09b3
Rajesh <158255395+Rajesh7094@users.noreply.github.com>
2024-08-04 22:43:14 +0530

    all added

---
+++ README.md
@@ -0,0 +1,75 @@
+# Startup-Company-Predicting-Machine-Learning with Streamlit 
+
+"Startup-Company-Prediction-Machine-Learning with Streamlit" likely refers to a project or initiative where a startup company will Succeed or not, a Python library for creating web applications, to showcase or deploy machine learning models.
+
+## Demo WebApp
+
+This app is up and running on Streamlit cloud server!!! Thanks 'Streamlit' for the community support for the cloud upload. You can check the demo of this web application on the link below.
+
+[Startup-Company-Prediction-Machine-Learning-With-Streamlit](https://github.com/Rajesh7094/Startup_Prediction)
+
+## Project Overview:
+
+#### Mr. Rajesh have successfully completed a groundbreaking project over , focusing on Startup-Company-Machine-Learning with Streamlit . This collaborative effort showcases their expertise in  machine learning, and software development.
+
+
+
+###  If you have any doubt or want to contribute feel free to hit me up on (rajesherode2004@gmail.com)
+
+# Startup-Company-Machine-Learning with Streamlit Demo
+
+### Login-Page
+
+![demo](https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExeThnbDQ5cmVrYnBkcGp2dHZkdWQybjFpOXVjNHduaGR2dnRnYjRjMyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/BuiWalMAqdDgv7IYBK/giphy.gif
+)
+
+### Startup Prediction Performance
+
+![demo](https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZzd4Mmd3dXQ1cm9rM2lzemx5ajBzcmF3dDU0dDBjcTFpbXU5bDcwaCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/qLnwPvnST1VznZyhb8/giphy.gif)
+
+### Startup Dashboard Performance 
+
+![demo](
+https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZmRjbWJ0ZWJnb2R2MWl5cWpkeWVpdnJjdW80a2N6d3M4dDdxNWc3aCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/SsrxpmCAbUPDzrM236/giphy.gif)
+
+## Requirements
+Python 3.9.13
+Streamlit
+Machine Learning
+Pandas
+Numpy
+Scikit-learn
+
+## How to Run
+* Clone the repository
+
+```
+$ git clone https://github.com/Hirthick6/Startup-Company-Machine-Learning.git
+```
+* Setup Virtual Environment
+
+```
+$ python3 -m venv env
+```
+* Activate the virtual environment
+
+```
+$ env\Scripts\activate
+```
+* Install dependencies using
+
+```
+$ pip install -r requirements.txt
+```
+* Run Streamlit
+
+```
+$ streamlit run main.py
+```
+</br>
+
+
+## Acknowledgements
+If you have any doubt or want to contribute feel free to hit me up on (hirthicksofficial@gmail.com)
+The app uses the Streamlit(<https://github.com/streamlit/streamlit>) library for the user interface.**
+
---
+++ dashboard.py
@@ -0,0 +1,88 @@
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+
+
+
+# Title of the app
+st.title("Data Visualization App")
+
+# Add a sidebar
+st.sidebar.subheader("Visualization Settings")
+
+# Setup file upload
+uploaded_file = st.sidebar.file_uploader(
+    label="Upload your CSV or Excel file. (200MB max)",
+    type=['csv', 'xlsx']
+)
+
+if uploaded_file is not None:
+    try:
+        df = pd.read_csv(uploaded_file)
+    except Exception as e:
+        print(e)
+        df = pd.read_excel(uploaded_file)
+
+    try:
+        st.write(df)
+        numeric_columns = list(df.select_dtypes(['float', 'int']).columns)
+        non_numeric_columns = list(df.select_dtypes(['object']).columns)
+        non_numeric_columns.append(None)
+    except Exception as e:
+        print(e)
+        st.write("Please upload a valid file to the application.")
+else:
+    st.write("Please upload a file to the application and Select Chart.")
+
+if uploaded_file is not None:
+    # Add a select widget to the sidebar
+    chart_select = st.sidebar.selectbox(
+        label="Select the chart type",
+        options=['Scatterplots', 'Lineplots', 'Histogram', 'Boxplot']
+    )
+
+    if chart_select == 'Scatterplots':
+        st.sidebar.subheader("Scatterplot Settings")
+        try:
+            x_values = st.sidebar.selectbox('X axis', options=numeric_columns)
+            y_values = st.sidebar.selectbox('Y axis', options=numeric_columns)
+            color_value = st.sidebar.selectbox("Color", options=non_numeric_columns)
+            plot = px.scatter(data_frame=df, x=x_values, y=y_values, color=color_value)
+            # Display the chart
+            st.plotly_chart(plot)
+        except Exception as e:
+            print(e)
+
+    if chart_select == 'Lineplots':
+        st.sidebar.subheader("Line Plot Settings")
+        try:
+            x_values = st.sidebar.selectbox('X axis', options=numeric_columns)
+            y_values = st.sidebar.selectbox('Y axis', options=numeric_columns)
+            color_value = st.sidebar.selectbox("Color", options=non_numeric_columns)
+            plot = px.line(data_frame=df, x=x_values, y=y_values, color=color_value)
+            st.plotly_chart(plot)
+        except Exception as e:
+            print(e)
+
+    if chart_select == 'Histogram':
+        st.sidebar.subheader("Histogram Settings")
+        try:
+            x = st.sidebar.selectbox('Feature', options=numeric_columns)
+            bin_size = st.sidebar.slider("Number of Bins", min_value=10, max_value=100, value=40)
+            color_value = st.sidebar.selectbox("Color", options=non_numeric_columns)
+            plot = px.histogram(data_frame=df, x=x, nbins=bin_size, color=color_value)
+            st.plotly_chart(plot)
+        except Exception as e:
+            print(e)
+
+    if chart_select == 'Boxplot':
+        st.sidebar.subheader("Boxplot Settings")
+        try:
+            y = st.sidebar.selectbox("Y axis", options=numeric_columns)
+            x = st.sidebar.selectbox("X axis", options=non_numeric_columns)
+            color_value = st.sidebar.selectbox("Color", options=non_numeric_columns)
+            plot = px.box(data_frame=df, y=y, x=x, color=color_value)
+            st.plotly_chart(plot)
+        except Exception as e:
+            print(e)
+
